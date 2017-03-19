2f94a673cef3880ab40def498f2bea3a5a785ae8
Staffan Olsson <staffan@repos.se>
2017-03-19 11:50:12 +0100

    An idea on how to plug in XML serialization without mssing with internal snapshots

--- server.js
+++ server.js
@@ -5,6 +5,8 @@ import http from 'http'
 import { Server as WebSocketServer } from 'ws'
 import seed from './seed'
 
+const xmlCompatibility = require('./xml-compatibility');
+
 /*
   CollabServerPackage provides an in-memory backend for testing purposes.
   For real applications, please provide a custom package here, which configures
@@ -28,6 +30,7 @@ let wss = new WebSocketServer({ server: httpServer })
 var documentServer = new DocumentServer({
   configurator: cfg
 })
+xmlCompatibility.patchDocumentServer(documentServer);
 documentServer.bind(app)
 
 /*
